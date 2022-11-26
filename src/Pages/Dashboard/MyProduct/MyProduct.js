82fcfc47c2878f686b412357e8220eda5a391183
bidduthosen <mdbidduthosen34@gmail.com>
2022-11-27 02:37:24 +0600

    add my product fetch data loaded

--- src/Pages/Dashboard/MyProduct/MyProduct.js
+++ src/Pages/Dashboard/MyProduct/MyProduct.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../Context/AuthProvider';
+import MyProductCard from './MyProductCard';
 
 const MyProduct = () => {
+    const {user} = useContext(AuthContext);
+    const {data: myProducts} = useQuery({
+        queryKey: ["addAProduct", user?.email],
+        queryFn: async()=>{
+            const res = await fetch(`http://localhost:5000/addAProduct?email=${user?.email}`)
+            const data = await res.json();
+            return data;
+        }
+    })
     return (
         <div>
-            <h1>my product</h1>
+            {
+                myProducts?.map(Product => <MyProductCard
+                    key={Product._id}
+                    product={Product}
+                ></MyProductCard>)
+            }
         </div>
     );
 };
